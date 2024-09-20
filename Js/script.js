576a60c40704a0562abf0829bd41096656cd784c
Arthur Henry Martins Brito <131808877+aTu44@users.noreply.github.com>
2024-09-20 17:02:29 -0300

    Delete Js directory

--- Js/script.js
+++
@@ -1,4 +0,0 @@
-const botão__header__responsivo = document.querySelector(".botão__header__responsivo");
-const cabecalho__menu = document.querySelector(".cabecalho__menu");
-
-botão__header__responsivo.addEventListener("click", () => cabecalho__menu.classList.toggle("active"));
